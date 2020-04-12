e29d0a1a5e51be9bd75eb03188999a712f295826
Mmanna3 <matiasmannarino@gmail.com>
2020-04-11 23:21:17 -0300

    Reedita migración anterior editada por error

--- Liga/LigaSoft/Migrations/202004112115122_CreacionDeRolDelegado.cs
+++ Liga/LigaSoft/Migrations/202004112115122_CreacionDeRolDelegado.cs
@@ -7,7 +7,6 @@ namespace LigaSoft.Migrations
     {
         public override void Up()
         {
-	        Sql("INSERT INTO [dbo].[AspNetRoles] ([Id], [Name]) VALUES ('2102983b-31df-46d7-8d11-30b244e9a65a', 'Delegado')");
 		}
         
         public override void Down()
