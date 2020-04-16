f3d01d0b10ee3fba806d91d30b5d6af8a27ae833
Mmanna3 <matiasmannarino@gmail.com>
2020-04-16 18:24:46 -0300

    Algunos test para GetForGrid

---
+++ Liga/LigaSoft/ExtensionMethods/ObjectExtension.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using Google.Apis.Util;
+
+namespace LigaSoft.ExtensionMethods
+{
+    public static class Object
+    {
+	    public static object GetReflectedProperty(this object obj, string propertyName)
+	    {
+		    obj.ThrowIfNull("obj");
+		    propertyName.ThrowIfNull("propertyName");
+
+		    PropertyInfo property = obj.GetType().GetProperty(propertyName);
+
+		    if (property == null)
+		    {
+			    return null;
+		    }
+
+		    return property.GetValue(obj, null);
+	    }
+	}
+}
---
+++ Liga/Tests/Integration/GetForGridTests.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using LigaSoft.Controllers;
+using LigaSoft.ExtensionMethods;
+using LigaSoft.Models.ViewModels;
+using NUnit.Framework;
+
+namespace Tests.Integration
+{
+	[TestFixture]
+	public class GetForGridTests : BaseIntegrationTest
+	{
+	    private readonly ClubController _ABMController;
+		private readonly int _totalDeClubesEnLaBase;
+		private readonly string _nombrePrimerClubSegunOrdenAlfabetico;
+		private readonly string _nombreUltimoClubSegunOrdenAlfabetico;
+
+		public GetForGridTests()
+	    {
+		    _nombrePrimerClubSegunOrdenAlfabetico = "Boca";
+		    _nombreUltimoClubSegunOrdenAlfabetico = "Velez";
+		    _ABMController = new ClubController();
+		    _totalDeClubesEnLaBase = Context.Clubs.Count();
+	    }
+
+		[Test]
+	    public void SinParametrizacionDevuelveTodosLosRegistros()
+		{
+			var result = _ABMController.GetForGrid(1, _totalDeClubesEnLaBase, null, null, null, null, null, null, null);
+			var clubs = (List<ClubVM>) result.Data.GetReflectedProperty("records");
+
+			Assert.AreEqual(clubs.Count, _totalDeClubesEnLaBase);
+	    }
+
+		[Test]
+		public void DevuelveTodosLosRegistrosOrdenadosAlfabeticamenteAscendente()
+		{
+			var result = _ABMController.GetForGrid(1, _totalDeClubesEnLaBase, "Nombre", "asc", null, null, null, null, null);
+			var clubs = (List<ClubVM>)result.Data.GetReflectedProperty("records");
+
+			Assert.AreEqual(clubs.First().Nombre, _nombrePrimerClubSegunOrdenAlfabetico);
+			Assert.AreEqual(clubs.Last().Nombre, _nombreUltimoClubSegunOrdenAlfabetico);
+		}
+
+		[Test]
+		public void DevuelveTodosLosRegistrosOrdenadosAlfabeticamenteDescendente()
+		{
+			var result = _ABMController.GetForGrid(1, _totalDeClubesEnLaBase, "Nombre", "desc", null, null, null, null, null);
+			var clubs = (List<ClubVM>)result.Data.GetReflectedProperty("records");
+
+			Assert.AreEqual(clubs.First().Nombre, _nombreUltimoClubSegunOrdenAlfabetico);
+			Assert.AreEqual(clubs.Last().Nombre, _nombrePrimerClubSegunOrdenAlfabetico);
+		}
+	}
+}
