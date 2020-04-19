482b95a23b65510794215f00f46a075d6ec5685c
Mmanna3 <matiasmannarino@gmail.com>
2020-04-19 12:39:38 -0300

    Tests escudos: guardar y eliminar

--- Liga/Tests/Unit/ImagenesEscudosDiskPersistenceTest.cs
+++ Liga/Tests/Unit/ImagenesEscudosDiskPersistenceTest.cs
@@ -1,11 +1,9 @@
-﻿using System.IO;
-using System.Linq;
-using System.Web.Hosting;
+﻿using System;
+using System.IO;
+using System.Web;
 using LigaSoft.Models.ViewModels;
 using LigaSoft.Utilidades.Persistence.DiskPersistence;
-using LigaSoft.ViewModelMappers;
 using NUnit.Framework;
-using Tests.Integration;
 
 namespace Tests.Unit
 {
@@ -13,21 +11,15 @@ namespace Tests.Unit
 	public class ImagenesEscudosDiskPersistenceTest
 	{
 		private readonly AppPathsForTest _paths;
-		private readonly ImagenesEscudosDiskPersistence _imagenesJugadoresDiskPersistence;
+		private readonly ImagenesEscudosDiskPersistence _imagenesEscudosDiskPersistence;
 		private const int CLUBID = 1;
-		private static JugadorBaseVM _jugadorBaseVm;
 		private static string _imagePath;
 
 		public ImagenesEscudosDiskPersistenceTest()
 		{
 			_paths = new AppPathsForTest();
-			_imagenesJugadoresDiskPersistence = new ImagenesEscudosDiskPersistence(_paths);
+			_imagenesEscudosDiskPersistence = new ImagenesEscudosDiskPersistence(_paths);
 			_imagePath = $"{_paths.ImagenesEscudosAbsolute}/{CLUBID}.jpg";
-			//_jugadorBaseVm = new CargarEscudoVM
-			//{
-			//	Escudo = ,
-			//	ClubId = CLUBID
-			//};
 		}
 
 		[SetUp]
@@ -49,21 +41,78 @@ namespace Tests.Unit
 		[Test]
 		public void Guardar()
 		{
-			//Assert.AreEqual(false, File.Exists(_imagePath));
-
-			//_imagenesJugadoresDiskPersistence.GuardarFotoWebCam(_jugadorBaseVm);
-
-			//Assert.AreEqual(true, File.Exists(_imagePath));
+			GuardarEscudoRandomEnDisco(CLUBID);
+			Assert.AreEqual(true, File.Exists(_imagePath));
 		}
 
 		[Test]
 		public void Eliminar()
 		{
+			GuardarEscudoRandomEnDisco(CLUBID);
+
+			_imagenesEscudosDiskPersistence.Eliminar(CLUBID);
+
+			Assert.AreEqual(false, File.Exists(_imagePath));
+		}
+
+		[Test]
+		public void PathSiEscudoNoExiste()
+		{
 			//Guardar();
+		}
 
-			//_imagenesJugadoresDiskPersistence.Eliminar(DNI);
+		[Test]
+		public void PathSiEscudoExiste()
+		{
+			//GuardarEscudoRandomEnDisco(CLUBID);
+			//_imagenesEscudosDiskPersistence.Path(CLUBID);
+		}
 
-			//Assert.AreEqual(false, File.Exists(_imagePath));
+		private void GuardarEscudoRandomEnDisco(int clubId)
+		{
+			var testImageFile = new HttpPostedFileRandomJpg();
+
+			var vm = new CargarEscudoVM
+			{
+				ClubId = clubId,
+				Escudo = testImageFile
+			};
+
+			_imagenesEscudosDiskPersistence.Guardar(vm);
+		}
+	}
+
+	internal class HttpPostedFileRandomJpg : HttpPostedFileBase
+	{
+		private readonly Stream stream;
+
+		private static MemoryStream GetRandomStream()
+		{
+			var byteBuffer = new byte[10];
+			var rnd = new Random();
+			rnd.NextBytes(byteBuffer);
+			return new MemoryStream(byteBuffer);
+		}
+
+		public HttpPostedFileRandomJpg()
+		{
+			stream = GetRandomStream();
+			ContentType = "image/jpeg";
+			FileName = "test-file.jpg";
+		}
+
+		public override int ContentLength => (int)stream.Length;
+
+		public override string ContentType { get; }
+
+		public override string FileName { get; }
+
+		public override Stream InputStream => stream;
+
+		public override void SaveAs(string filename)
+		{
+			using (var file = File.Open(filename, FileMode.CreateNew))
+				stream.CopyTo(file);
 		}
 	}
 }
