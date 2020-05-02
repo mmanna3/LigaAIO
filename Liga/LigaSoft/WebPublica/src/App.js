e5888a9f3eabeee7a9824a5a5a65fa7f657d5c55
Mmanna3 <matiasmannarino@gmail.com>
2020-05-02 15:26:24 -0300

    Revierte cambio de prueba en webpublica

--- Liga/LigaSoft/WebPublica/src/App.js
+++ Liga/LigaSoft/WebPublica/src/App.js
@@ -13,7 +13,7 @@ function App() {
         <div className="app">
           <BannerHome titulo="Torneos" color={COLOR.ROJO} />
           <BannerHome titulo="Noticias" color={COLOR.VERDE} />
-          <BannerHome titulo="Prueba" color={COLOR.AZUL} />
+          <BannerHome titulo="Contacto" color={COLOR.AZUL} />
         </div>
       )
     else if (seccionPrincipalSeleccionada == "Torneos")
