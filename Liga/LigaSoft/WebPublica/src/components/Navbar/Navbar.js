8b27176a605942afb26a3df0c16b322a141ad6c5
Mmanna3 <matiasmannarino@gmail.com>
2020-05-04 00:03:46 -0300

    Navbar

--- Liga/LigaSoft/WebPublica/src/components/Navbar/Navbar.js
+++ Liga/LigaSoft/WebPublica/src/components/Navbar/Navbar.js
@@ -1,9 +1,16 @@
 import React, { useState }  from "react";
 import styles from "./Navbar.css";
 import bootstrap from "GlobalStyle/bootstrap.min.css";
+import {actualizarSeccionPrincipal} from 'Store/seccion-principal/action';
+import {actualizarTorneo} from 'Store/torneo/action';
+import {actualizarZona} from 'Store/zona/action';
+import {actualizarFase} from 'Store/fase/action';
+import {actualizarOpcion} from 'Store/opcion/action';
+import {useDispatch} from 'react-redux';
 
 const Navbar = () => {
 const [desplegarMenu, setDesplegarMenu] = useState(false);
+const dispatch = useDispatch();
 
 const menuClass = () => {
     if (desplegarMenu)
@@ -12,35 +19,47 @@ const menuClass = () => {
         return styles.menuSinDesplegar;
 };
 
-  return (
-            <nav className={styles.navbarYkn}>
-                <div className={bootstrap.container}>
-                    <a className={bootstrap['navbar-brand']} href="/publico/index">
-                        <img className={styles.logoEdefi} alt="EDEFI" />
-                    </a>
+const actualizarSeccionPrincipalYOcultarMenu = (seccionPrincipal) => {
+    setDesplegarMenu(false); 
+    dispatch(actualizarSeccionPrincipal(seccionPrincipal));
+    dispatch(actualizarTorneo(""));
+    dispatch(actualizarZona(""));
+    dispatch(actualizarFase(""));
+    dispatch(actualizarOpcion(""));
+}
+
+return (
+        <nav className={styles.navbarYkn}>
+            <div className={bootstrap.container}>
+                <a className={bootstrap['navbar-brand']} href="#" onClick={() => actualizarSeccionPrincipalYOcultarMenu("")}>
+                    <img className={styles.logoEdefi} alt="EDEFI" />
+                </a>
 
 
-                    <button className={bootstrap['navbar-toggler']} type="button" data-toggle="collapse" onClick={() => setDesplegarMenu(!desplegarMenu)}>
-                        <span className={styles['navbarTogglerIcon']}></span>
-                    </button>
+                <button className={bootstrap['navbar-toggler']} type="button" data-toggle="collapse" onClick={() => setDesplegarMenu(!desplegarMenu)}>
+                    <span className={styles['navbarTogglerIcon']}></span>
+                </button>
 
-                    <div className={menuClass()}>
-                        <ul className={bootstrap['navbar-nav']}>
-                            <li className={bootstrap['nav-item']}>
-                                <a className={styles.navLink} href="#">Home</a>
-                            </li>
-                            <li className={bootstrap['nav-item']}>
-                                <a className={styles.navLink} href="#">Noticias</a>
-                            </li>
-                            <li className={bootstrap['nav-item']}>
-                                <a className={styles.navLink} href="#">Contacto</a>
-                            </li>
-                        </ul>
-                    </div>
-
+                <div className={menuClass()}>
+                    <ul className={bootstrap['navbar-nav']}>
+                        <li className={bootstrap['nav-item']}>
+                            <a className={styles.navLink} href="#" onClick={() => actualizarSeccionPrincipalYOcultarMenu("")}>Home</a>
+                        </li>
+                        <li className={bootstrap['nav-item']}>
+                            <a className={styles.navLink} href="#" onClick={() => actualizarSeccionPrincipalYOcultarMenu("Torneos")}>Torneos</a>
+                        </li>
+                        <li className={bootstrap['nav-item']}>
+                            <a className={styles.navLink} href="#" onClick={() => actualizarSeccionPrincipalYOcultarMenu("Noticias")}>Noticias</a>
+                        </li>
+                        <li className={bootstrap['nav-item']}>
+                            <a className={styles.navLink} href="#" onClick={() => actualizarSeccionPrincipalYOcultarMenu("Contacto")}>Contacto</a>
+                        </li>
+                    </ul>
                 </div>
-            </nav>
-      );
+
+            </div>
+        </nav>
+    );
 }
 
 export default Navbar;
