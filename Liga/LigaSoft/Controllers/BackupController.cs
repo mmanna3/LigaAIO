cbe367165bc136e85f40ece18d078d70a8beda16
Mmanna3 <matiasmannarino@gmail.com>
2020-05-15 00:34:36 -0300

    Prueba: generación de backup

--- Liga/LigaSoft/Controllers/BackupController.cs
+++ Liga/LigaSoft/Controllers/BackupController.cs
@@ -1,4 +1,8 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Diagnostics;
+using System.Web.Hosting;
+using System.Web.Mvc;
+using LigaSoft.Models;
 using LigaSoft.Scheduler;
 using LigaSoft.Utilidades;
 
@@ -12,6 +16,40 @@ namespace LigaSoft.Controllers
 		    BackupBaseDeDatosYFileSystem.GenerarYSubirADrive();
 
 			return Json("", JsonRequestBehavior.AllowGet);
+		}
+
+		public JsonResult Db()
+		{
+			LaunchCommandLineApp();
+
+			return Json("Generado?", JsonRequestBehavior.AllowGet);
+		}
+
+		private static void LaunchCommandLineApp()
+		{
+			var backupDirectory = HostingEnvironment.MapPath("~/App_Data/BackupZenSchema");
+			var connectionString = new ApplicationDbContext().Database.Connection.ConnectionString;
+
+			var startInfo = new ProcessStartInfo
+			{
+				CreateNoWindow = false,
+				UseShellExecute = false,
+				FileName = HostingEnvironment.MapPath("~/SchemaZen.exe") ?? throw new InvalidOperationException(),
+				WindowStyle = ProcessWindowStyle.Hidden,
+				Arguments = $@"script -c ""{connectionString}"" --dataTablesPattern "".*"" -d  ""{backupDirectory}"" -v -o"
+			};
+
+			try
+			{
+				using (var exeProcess = Process.Start(startInfo))
+				{
+					exeProcess?.WaitForExit();
+				}
+			}
+			catch
+			{
+				// Log error.
+			}
 		}
 	}
 }
