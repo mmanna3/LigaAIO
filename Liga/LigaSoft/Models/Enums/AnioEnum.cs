8a54a67cc46bd5a9a628dc30cbf07289ada39204
mmmannna <mmmannna3@gmail.com>
2021-01-20 22:57:20 -0300

    Agrega año 2021 a los torneos

--- Liga/LigaSoft/Models/Enums/AnioEnum.cs
+++ Liga/LigaSoft/Models/Enums/AnioEnum.cs
@@ -9,6 +9,8 @@ namespace LigaSoft.Models.Enums
 		[Display(Name = "2019")]
 		A2019 = 2019,
 		[Display(Name = "2020")]
-		A2020 = 2020
+		A2020 = 2020,
+		[Display(Name = "2021")]
+		A2021 = 2021
 	}
 }
