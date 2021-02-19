402cd981bba4a61fb150aa1f0db8d4932e9870ea
mmmannna <mmmannna3@gmail.com>
2021-02-19 00:41:08 -0300

    Toma los datos del backend y muestra correctamente las sanciones en una tabla

--- Liga/LigaSoft/WebPublica/src/components/SeccionPrincipalTorneos/Menu/OpcionSanciones/OpcionSanciones.js
+++ Liga/LigaSoft/WebPublica/src/components/SeccionPrincipalTorneos/Menu/OpcionSanciones/OpcionSanciones.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {fetchDataAndRenderResponse} from "Utils/hooks";
-import TablaSanciones from "./TablaSanciones/TablaSanciones";
+// import TablaSanciones from "./TablaSanciones/TablaSanciones";
+import tabla from './../../../../assets/styles/tabla.css';
 import styles from "./OpcionSanciones.css";
 
 const OpcionSanciones = (props) =>{
@@ -8,11 +9,34 @@ const OpcionSanciones = (props) =>{
   const render = (data) => {
 
       return (
-        <div className={styles.rowTablas}>
-          {data.Fechas.map(({ DiaDeLaFecha, Titulo, LocalVisitante }) => (
-            <TablaSanciones key={DiaDeLaFecha} diaDeLaFecha={DiaDeLaFecha} titulo={Titulo} renglones={LocalVisitante} />
-          ))}
-        </div>
+        
+              <table className={styles.tabla}>
+                <thead>
+                <tr>
+                  <th className={tabla.cabeceraIzquierda}>Día</th>
+                  <th className={tabla.cabeceraCentrada}>Fecha</th>
+                  <th className={tabla.cabeceraCentrada}>Categoría</th>
+                  <th className={tabla.cabeceraCentrada}>Local</th>
+                  <th className={tabla.cabeceraCentrada}>Visitante</th>
+                  <th className={tabla.cabeceraCentrada}>Sanción</th>
+                  <th className={tabla.cabeceraDerecha}>Fechas adeudadas</th>
+                </tr>
+                </thead>
+                <tbody>
+                  {data.map(({dia, fecha, categoria, local, visitante, sancion, fechasQueAdeuda}) => (
+                    <tr key={dia}>                      
+                      <td className={tabla.celdaIzquierda}>{dia}</td>
+                      <td className={tabla.celdaCentrada}>{fecha}</td>
+                      <td className={tabla.celdaCentrada}>{categoria}</td>
+                      <td className={tabla.celdaCentrada}>{local}</td>
+                      <td className={tabla.celdaCentrada}>{visitante}</td>
+                      <td className={tabla.celdaCentrada}>{sancion}</td>
+                      <td className={tabla.celdaDerecha}>{fechasQueAdeuda}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+
       )
     }    
 
