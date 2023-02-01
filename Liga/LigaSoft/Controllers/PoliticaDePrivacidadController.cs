918dda8937b6e18f4c53c0ca8dbf6e1847ae0f0d
mmmannna <mmmannna3@gmail.com>
2023-02-01 18:41:05 -0300

    Política de privacidad para Play store

---
+++ Liga/LigaSoft/Controllers/PoliticaDePrivacidadController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Dynamic;
+using System.Web.Mvc;
+using LigaSoft.BusinessLogic;
+using LigaSoft.Models.ViewModels;
+using LigaSoft.Utilidades;
+using LigaSoft.ViewModelMappers;
+using Newtonsoft.Json;
+using LigaSoft.Models;
+
+namespace LigaSoft.Controllers
+{
+	[AllowAnonymous]
+	public class PoliticaDePrivacidadController : Controller
+    {
+
+		[AllowAnonymous]
+		public ActionResult Index()
+		{
+			return View();
+		}
+	}
+}
