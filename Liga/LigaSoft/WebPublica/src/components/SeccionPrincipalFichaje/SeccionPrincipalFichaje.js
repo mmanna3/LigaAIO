18cc318070fa52025c8c8c4b4e1a51932f2f2dda
mmanna3 <mmmannna3@gmail.com>
2023-02-03 18:22:21 -0300

    Habilita fichaje

--- Liga/LigaSoft/WebPublica/src/components/SeccionPrincipalFichaje/SeccionPrincipalFichaje.js
+++ Liga/LigaSoft/WebPublica/src/components/SeccionPrincipalFichaje/SeccionPrincipalFichaje.js
@@ -60,7 +60,7 @@ const SeccionPrincipalFichaje = () => {
     const huboAlgunError = !(Object.keys(errors).length === 0 && errors.constructor === Object)
 
     // if (!estaLaSeccionHabilitada())
-    if (true)
+    if (false)
         return (
             <div className={bootstrap['row']}>
                 <div className={bootstrap['col-12']}>
