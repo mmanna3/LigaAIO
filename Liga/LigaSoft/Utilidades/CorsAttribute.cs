73e815b63836e2f839fa097a1a9364348d195d4a
mmmannna <mmmannna3@gmail.com>
2023-02-11 16:32:41 -0300

    Modifica cors

--- Liga/LigaSoft/Utilidades/CorsAttribute.cs
+++ Liga/LigaSoft/Utilidades/CorsAttribute.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Web.Mvc;
 
 public class AllowCrossSiteAttribute : ActionFilterAttribute
@@ -6,9 +7,15 @@ public class AllowCrossSiteAttribute : ActionFilterAttribute
     public override void OnActionExecuting(ActionExecutingContext filterContext)
     {
         filterContext.RequestContext.HttpContext.Response.AddHeader("Access-Control-Allow-Origin", "*");
-        filterContext.RequestContext.HttpContext.Response.AddHeader("Access-Control-Allow-Headers", "*");
+        filterContext.RequestContext.HttpContext.Response.AddHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         filterContext.RequestContext.HttpContext.Response.AddHeader("Access-Control-Allow-Credentials", "true");
+
+        if (filterContext.HttpContext.Request.HttpMethod == "OPTIONS")
+        {
+            filterContext.HttpContext.Response.Flush();
+        }
 
         base.OnActionExecuting(filterContext);
     }
+
 }
