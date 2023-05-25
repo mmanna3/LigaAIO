133395e0d3da9407a863f56af5dfc6863a0ee74c
mmmannna <mmmannna3@gmail.com>
2023-05-25 16:34:52 -0300

    Agrega migración para renombrar publicidades

---
+++ Liga/LigaSoft/Migrations/202305251923394_RenombraTitulosDePublicidadesADesktopYBanner.cs
@@ -0,0 +1,31 @@
+﻿namespace LigaSoft.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class RenombraTitulosDePublicidadesADesktopYBanner : DbMigration
+    {
+        public override void Up()
+        {
+            Sql(@"  update publicidad
+                    set titulo = 'Desktop'
+                    where id = 1
+
+                    update publicidad
+                    set titulo = 'Mobile'
+                    where id = 2
+
+                    update publicidad
+                    set titulo = 'No utilizado'
+                    where id = 3
+
+                    update publicidad
+                    set titulo = 'No utilizado'
+                    where id = 4");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
