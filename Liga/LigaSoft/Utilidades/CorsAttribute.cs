167c69f26dd502d70d5729de57aebab8b3f27cca
mmmannna <mmmannna3@gmail.com>
2023-05-26 23:59:04 -0300

    Cors: Prueba habilitando Allow-Methods para todos

--- Liga/LigaSoft/Utilidades/CorsAttribute.cs
+++ Liga/LigaSoft/Utilidades/CorsAttribute.cs
@@ -7,6 +7,7 @@ public class AllowCrossSiteAttribute : ActionFilterAttribute
     public override void OnActionExecuting(ActionExecutingContext filterContext)
     {
         filterContext.RequestContext.HttpContext.Response.AddHeader("Access-Control-Allow-Origin", "*");
+        filterContext.RequestContext.HttpContext.Response.AddHeader("Access-Control-Allow-Methods", "*");
         filterContext.RequestContext.HttpContext.Response.AddHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         filterContext.RequestContext.HttpContext.Response.AddHeader("Access-Control-Allow-Credentials", "true");
 
