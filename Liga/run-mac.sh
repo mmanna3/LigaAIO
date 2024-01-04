cd39c241ccb0c3e39810de1a6477b197daaf23e6
mmanna3 <mmmannna3@gmail.com>
2024-01-04 17:30:12 -0300

    Agrega script para levantar la app en mac

---
+++ Liga/run-mac.sh
@@ -0,0 +1,9 @@
+# Stop any servers running on port 5001
+ps aux | grep -i xsp4.*42000 | awk '{print $2}' | xargs kill -9
+
+cd LigaSoft && xsp4 --address 0.0.0.0 --port 42000 --nonstop & 
+
+# Espera para asegurarse que el servidor haya arrancado posta
+sleep 2s
+
+open http://0.0.0.0:42000/torneo &
